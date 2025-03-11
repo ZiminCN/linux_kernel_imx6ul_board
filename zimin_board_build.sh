680209636d3e1644265f75ac22d28e534e621a98
Zi Min <18171190533@163.com>
2025-03-11 21:54:25 +0800

    trival: modified bash script
    Signed-off-by: Zi Min <18171190533@163.com>

--- zimin_board_build.sh
+++ zimin_board_build.sh
@@ -1,8 +1,12 @@
 #!bin/bash
 
-make CROSS_COMPILE=arm-linux-gnueabihf- clean
-# make CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make CROSS_COMPILE=arm-linux-gnueabihf- imx6ul_board_defconfig
-make CROSS_COMPILE=arm-linux-gnueabihf- imx6ul-board-nand.dtb
-make CROSS_COMPILE=arm-linux-gnueabihf- zImage -j4
+export PATH=/home/zengjianming/compliation_tool/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin:$PATH
+
+# make CROSS_COMPILE=arm-linux-gnueabihf- clean -j12
+make CROSS_COMPILE=arm-linux-gnueabihf- distclean -j12
+
+# make CROSS_COMPILE=arm-linux-gnueabihf- imx6ul_board_defconfig -j12
+# make CROSS_COMPILE=arm-linux-gnueabihf- imx6ul-board-nand.dtb -j12
+# make CROSS_COMPILE=arm-linux-gnueabihf- zImage -j12
+
 # make CROSS_COMPILE=arm-linux-gnueabihf- modules -j4
