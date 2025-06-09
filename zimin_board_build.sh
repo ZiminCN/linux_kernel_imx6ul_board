efecb300d13362f00cc24ddcf95df803f8042331
zimin <18171190533@163.com>
2025-06-09 23:04:56 +0800

    feat: improve config setting
    Signed-off-by: zimin <18171190533@163.com>

--- zimin_board_build.sh
+++ zimin_board_build.sh
@@ -5,8 +5,8 @@ export PATH=/home/zengjianming/compliation_tool/gcc-linaro-7.5.0-2019.12-x86_64_
 # make CROSS_COMPILE=arm-linux-gnueabihf- clean -j12
 make CROSS_COMPILE=arm-linux-gnueabihf- distclean -j12
 
-# make CROSS_COMPILE=arm-linux-gnueabihf- imx6ul_board_defconfig -j12
-# make CROSS_COMPILE=arm-linux-gnueabihf- imx6ul-board-nand.dtb -j12
+# make CROSS_COMPILE=arm-linux-gnueabihf- imx6ul_zimin_board_sd_defconfig -j12
+# make CROSS_COMPILE=arm-linux-gnueabihf- imx6ul-zimin-board-sd.dtb -j12
 # make CROSS_COMPILE=arm-linux-gnueabihf- zImage -j12
 
 # make CROSS_COMPILE=arm-linux-gnueabihf- modules -j4
