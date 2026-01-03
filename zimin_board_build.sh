5cc47a02e4ff2dc5cea904d7f6f86d6ca8ac7169
zimin <18171190533@163.com>
2026-01-03 18:03:50 +0800

    feat: modified dts/defconfig file
    Signed-off-by: zimin <18171190533@163.com>

--- zimin_board_build.sh
+++ zimin_board_build.sh
@@ -2,11 +2,23 @@
 
 export PATH=/home/zengjianming/compliation_tool/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin:$PATH
 
-# make CROSS_COMPILE=arm-linux-gnueabihf- clean -j12
+make CROSS_COMPILE=arm-linux-gnueabihf- clean -j12
 make CROSS_COMPILE=arm-linux-gnueabihf- distclean -j12
 
 # make CROSS_COMPILE=arm-linux-gnueabihf- imx6ul_zimin_board_sd_defconfig -j12
 # make CROSS_COMPILE=arm-linux-gnueabihf- imx6ul-zimin-board-sd.dtb -j12
 # make CROSS_COMPILE=arm-linux-gnueabihf- zImage -j12
 
-# make CROSS_COMPILE=arm-linux-gnueabihf- modules -j4
+# sudo mount /dev/sda1 /mnt/imx6ul-ws/boot
+# sudo mount /dev/sda2 /mnt/imx6ul-ws/rootfs
+
+# make CROSS_COMPILE=arm-linux-gnueabihf- modules -j4 
+# sudo make CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=/mnt/imx6ul-ws/rootfs modules_install
+
+# sudo cp arch/arm/boot/zImage /mnt/imx6ul-ws/boot/zImage
+# sudo cp arch/arm/boot/dts/imx6ul-zimin-board-sd.dtb /mnt/imx6ul-ws/boot/imx6ul-zimin-board-sd.dtb
+
+# sudo sync
+
+# sudo umount /mnt/imx6ul-ws/boot
+# sudo umount /mnt/imx6ul-ws/rootfs
